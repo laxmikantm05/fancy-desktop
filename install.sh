23bf1b20d32738c42867230df934397a1787e571
Laxmikant <laxmikantmaity1944@gmail.com>
2026-03-13 23:43:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,12 +46,12 @@ log "Setting things up for you, sit back and relax..."
 case "$PKG_MANAGER" in
     apt)
         log "Preparing your desktop..."
-        cd ~/fancy-desktop/script/debian/
+        cd ~/fancy-desktop/script/
 		bash debian-rice.sh
         ;;
     dnf)
         log "Preparing your desktop..."
-        cd ~/fancy-desktop/script/fedora/
+        cd ~/fancy-desktop/script/
 		bash fedora-rice.sh
         ;;
     *)
